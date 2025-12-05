254c2d50fd9187201f9eb6b725d9ab1f5b12e26d
Cauteros974 <pusik6666@ukr.net>
2025-12-05 20:48:33 +0200

    Update RecipeContext.jsx

--- src/context/RecipeContext.jsx
+++ src/context/RecipeContext.jsx
@@ -10,5 +10,13 @@ const MOCK_RECIPED = [
         image: 'https://images.unsplash.com/photo-1604382354936-07c5d9983bd3?w=800',
         time: 40,
         author: Mia,
+        likes: 120,
+    },
+    {
+        id: 2,
+        title: 'Avocado toast',
+        image: 'https://images.unsplash.com/photo-1541519227354-08fa5d50c44d?w=800',
+        time: 10,
+        author: 'HipsterChef',
     }
 ]
