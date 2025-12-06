9447b27985f67064c1b2583c1ff796acbba0cbfd
Cauteros974 <pusik6666@ukr.net>
2025-12-06 20:01:13 +0200

    Update Feed.jsx

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -6,4 +6,12 @@ const Feed = () => {
     const {recipes, loading } = useContext(RecipeContext);
 
     if(loading) return <div style={{ padding: 20, textAlign: 'center'}}>Loading goodies... 🍳</div>
-}
+
+    return(
+        <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px' }}>
+            <h1 style={{ textAlign: 'center', marginBottom: '30px' }}>Recipe feed</h1>
+        </div>
+    )
+};
+
+export default Feed;
