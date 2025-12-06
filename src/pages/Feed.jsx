c3acff30f2e0c19e39f449c00655da979bb9c172
Cauteros974 <pusik6666@ukr.net>
2025-12-06 20:00:10 +0200

    Update Feed.jsx

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -3,5 +3,7 @@ import { RecipeContext } from "../context/RecipeContext";
 import RecipeCard from "../components/RecipeCard";
 
 const Feed = () => {
-    const {recipes, loading } = useContext(RecipeCard);
+    const {recipes, loading } = useContext(RecipeContext);
+
+    if(loading) return <div style={{ padding: 20, textAlign: 'center'}}>Loading goodies... 🍳</div>
 }
