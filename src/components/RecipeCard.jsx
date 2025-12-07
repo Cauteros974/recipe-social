8efb57905b23b173ac4f8ca701d2cdc5e762d5e6
Cauteros974 <pusik6666@ukr.net>
2025-12-07 11:16:11 +0200

    Update RecipeCard.jsx

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -8,19 +8,19 @@ const RecipeCard = ({recipe}) => {
 
     return(
         <div className={style.card}>
-            <img src={recipe.image} alt={recipe.title} className={styles.image} />
-            <div className={styles.content}>
-                <h3 className={styles.title}>{recipe.title}</h3>
+            <img src={recipe.image} alt={recipe.title} className={style.image} />
+            <div className={style.content}>
+                <h3 className={style.title}>{recipe.title}</h3>
                 <p className={{marginBottom: '10px', color: '#888'}}>From: {recipe.author}</p>
 
-                <div className={styles.meta}>
+                <div className={style.meta}>
                     <div style={{display: 'flex', alignItems: 'center', gap: '5px'}}>
                         <Clock size={16} />
                         {recipe.time} Min
                     </div>
 
                     <button
-                        className={styles.button}
+                        className={style.button}
                         onClick={() => toggleLike(recipe.id)}
                     >
                         <Heart size={20} /> {recipe.likes}
