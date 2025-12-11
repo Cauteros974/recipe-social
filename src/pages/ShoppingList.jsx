01e38685349042b9fa63ed4b5beb41489fb5d966
Cauteros974 <pusik6666@ukr.net>
2025-12-11 21:17:49 +0200

    Update ShoppingList.jsx

--- src/pages/ShoppingList.jsx
+++ src/pages/ShoppingList.jsx
@@ -9,8 +9,22 @@ const ShoppingList = () => {
         <div style={{ maxWidth: '600px', margin: '20px auto', padding: '20px', background: 'white', borderRadius: '15px', boxShadow: '0 4px 12px rgba(0,0,0,0.05)'}}>
             <h2 style={{ textAlign: 'center', marginBottom: '20px'}}>🛒 Shopping List</h2>
 
-            {shoppingList.length === 0 ? (
+            {shoppingList.lenght === 0 ? (
                 <p style={{ textAlign: 'center', color: '#888' }}>The list is empty. Add ingredients from recipes!</p>
+            ): (
+                <ul style={{listStyle: 'none', padding: 0}}>
+                    {shoppingList.map(item => (
+                        <li key={item.id} style={{ 
+                            display: 'flex', 
+                            alignItems: 'center', 
+                            justifyContent: 'space-between',
+                            padding: '12px',
+                            borderBottom: '1px solid #eee',
+                            opacity: item.completed ? 0.5 : 1
+                        }}>
+
+                        </li>
+            }}>
             )}
         </div>
     );
