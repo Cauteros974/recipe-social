6a063549c18881752699a77d95dad545e8f0d68b
Cauteros974 <pusik6666@ukr.net>
2025-12-11 21:10:19 +0200

    Create ShoppingList.jsx

---
+++ src/pages/ShoppingList.jsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import RecipeCard from "../components/RecipeCard";
+import { Trash2, CheckSquare, Square } from "lucide-react";
+
+const ShoppingList = () => {
+    const { shoppingList, toggleShoppingItem, removeShoppingItem } = useContext(RecipeContext);
+
+    return(
+        <div style={{ maxWidth: '1200px', margin: '20px auto', padding: '20px', background: 'white', borderRadius: '15px', boxShadow: '0 4px 12px rgba(0,0,0,0.05)'}}>
+            
+        </div>
+    )
+};
+
+export default ShoppingList
