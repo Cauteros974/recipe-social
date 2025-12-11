4a71183ad6f55be062de055e5530227106d38d46
Cauteros974 <pusik6666@ukr.net>
2025-12-11 21:14:17 +0200

    Update ShoppingList.jsx

--- src/pages/ShoppingList.jsx
+++ src/pages/ShoppingList.jsx
@@ -7,9 +7,13 @@ const ShoppingList = () => {
 
     return(
         <div style={{ maxWidth: '600px', margin: '20px auto', padding: '20px', background: 'white', borderRadius: '15px', boxShadow: '0 4px 12px rgba(0,0,0,0.05)'}}>
+            <h2 style={{ textAlign: 'center', marginBottom: '20px'}}>🛒 Shopping List</h2>
 
+            {shoppingList.length === 0 ? (
+                <p style={{ textAlign: 'center', color: '#888' }}>The list is empty. Add ingredients from recipes!</p>
+            )}
         </div>
-    )
+    );
 };
 
-export default ShoppingList
+export default ShoppingList;
