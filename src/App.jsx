6f54707e2b772256d0b9fa3a9337701290e86492
Cauteros974 <pusik6666@ukr.net>
2025-12-13 20:01:48 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
-import { Routes, Route } from "react-router-dom";
-import { RecipeProvider } from "./context/RecipeContext";
-import Navbar from "./components/Navbar";
-import Feed from "./pages/Feed";
-import CreateRecipe from "./pages/CreateRecipe";
-import RecipeDetail from "./pages/RecipeDetail";
+import { Routes, Route } from 'react-router-dom';
+import { RecipeProvider } from './context/RecipeContext';
+import Navbar from './components/Navbar';
+import Feed from './pages/Feed';
+import CreateRecipe from './pages/CreateRecipe';
+import RecipeDetail from './pages/RecipeDetail';
+import ShoppingList from './pages/ShoppingList';
 
 function App() {
   return (
+    
     <RecipeProvider>
-      <div style={{ minHeight: '100vh', background: '#f8f9fa' }}>
+      <div style={{ minHeight: '100vh', background: '#f8f9fa', fontFamily: 'sans-serif' }}>
+        
+        {/* Navigation (Navbar) is always on top */}
         <Navbar />
+
+        {/* Content varies depending on URL */}
         <div style={{ paddingBottom: '50px' }}>
           <Routes>
             <Route path="/" element={<Feed />} />
             <Route path="/create" element={<CreateRecipe />} />
             <Route path="/recipe/:id" element={<RecipeDetail />} />
+            <Route path="/shopping-list" element={<ShoppingList />} />
           </Routes>
         </div>
+
       </div>
     </RecipeProvider>
   );
